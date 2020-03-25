e761d9fd9881cd938d8280342a039ddf3dd5c410
Chrisctr <credfern96@gmail.com>
2020-03-25 13:02:59 +0000

    Added java files

---
+++ src/unique/sum/exercise/Runner.java
@@ -0,0 +1,13 @@
+package unique.sum.exercise;
+
+	public class Runner {
+		
+		public static void main(String[] args) {
+		
+			UniqueSum sumCalculator = new UniqueSum();
+			System.out.println(sumCalculator.input(5, 4, 23));
+			
+			
+		}
+				
+	}
---
+++ src/unique/sum/exercise/UniqueSum.java
@@ -0,0 +1,12 @@
+package unique.sum.exercise;
+
+	public class UniqueSum {
+		
+		public int input(int num1, int num2, int num3) {
+			
+			System.out.println(num1 + " + " + num2 + " + " + num3 + " = ");
+			return num1 + num2 + num3;
+						
+		}
+				
+	}
