e046c24807359e58c0836a2d1acd4e24c02de674
Chrisctr <credfern96@gmail.com>
2020-03-26 13:47:21 +0000

    Improved (fixed) Unique Sum exercise

--- src/unique/sum/exercise/Runner.java
+++ src/unique/sum/exercise/Runner.java
@@ -1,13 +1,12 @@
 package unique.sum.exercise;
 
-	public class Runner {
-		
-		public static void main(String[] args) {
-		
-			UniqueSum sumCalculator = new UniqueSum();
-			System.out.println(sumCalculator.input(5, 4, 23));
-			
-			
-		}
-				
+public class Runner {
+
+	public static void main(String[] args) {
+
+		UniqueSum sumCalculator = new UniqueSum();
+		System.out.println(sumCalculator.input(2, 3, 3));
+
 	}
+
+}
--- src/unique/sum/exercise/UniqueSum.java
+++ src/unique/sum/exercise/UniqueSum.java
@@ -1,12 +1,40 @@
 package unique.sum.exercise;
 
-	public class UniqueSum {
-		
-		public int input(int num1, int num2, int num3) {
-			
-			System.out.println(num1 + " + " + num2 + " + " + num3 + " = ");
-			return num1 + num2 + num3;
-						
+public class UniqueSum {
+
+	public String input(int num1, int num2, int num3) {
+
+		int answer = 0;
+		String answerString = "";
+
+		if (num1 != num2 && num2 != num3 && num1 != num3) {
+			answerString = num1 + " + " + num2 + " + " + num3 + " = ";
+			return (answerString + answer);
 		}
-				
+		if (num1 == num2 && num1 == num3 && num2 == num3) {
+			num1 = 0;
+			num2 = 0;
+			num3 = 0;
+			answerString = "All the same. Nothin' to see here, folks";
+			return (answerString);
+		}
+		if (num2 == num3) {
+			num2 = 0;
+			num3 = 0;
+			answerString = "Only one number's unique: " + num1;
+		}
+		if (num1 == num2) {
+			num1 = 0;
+			num2 = 0;
+			answerString = "Only one number's unique: " + num3;
+		}
+		if (num1 == num3) {
+			num1 = 0;
+			num3 = 0;
+			answerString = "Only one number's unique: " + num2;
+		}
+
+		return (answerString);
 	}
+
+}
